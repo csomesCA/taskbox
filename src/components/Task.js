f5fe5a9f617ef288e6bdd1bf35446beae60b9492
Colton Somes <csomes@commerce-architects.com>
2020-10-02 15:31:16 -0700

    Added Story for long title

--- src/components/Task.js
+++ src/components/Task.js
@@ -19,7 +19,7 @@ export default function Task({task: {id, title, state}, onArchiveTask, onPinTask
                     value={title} 
                     readOnly={true} 
                     placeholder="Input title"
-                    style={{background: 'red'}}
+                    style={{textOverflow: 'ellipsis'}}
                 />
             </div>
 
