fdbdc5b49eb6572274d3289bc995f6eecf70d407
Colton Somes <csomes@commerce-architects.com>
2020-10-02 14:12:30 -0700

    change task background to red

--- src/components/Task.js
+++ src/components/Task.js
@@ -14,7 +14,13 @@ export default function Task({task: {id, title, state}, onArchiveTask, onPinTask
                 <span className="checkbox-custom" onClick={() => onArchiveTask(id)} />
             </label>
             <div className="checkbox-custom" onClick={() => onArchiveTask(id)} >
-                <input type="text" value={title} readOnly={true} placeholder="Input title"/>
+                <input 
+                    type="text" 
+                    value={title} 
+                    readOnly={true} 
+                    placeholder="Input title"
+                    style={{background: 'red'}}
+                />
             </div>
 
             <div className="actions" onClick={event => event.stopPropagation()}>
